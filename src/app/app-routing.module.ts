c1608556f9048e0a22c1f1e2a7e929b937118617
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-05-16 23:39:58 +0530

    Initial Project Files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+{
+  path:'login', component:UserComponent
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
