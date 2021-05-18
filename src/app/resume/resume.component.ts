7be962585454de99163818726623c166fb9dd870
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-05-18 12:58:21 +0530

    Pdf Download of resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-resume',
@@ -12,4 +14,21 @@ export class ResumeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  generatePDF() {
+    var data = document.getElementById('resume') as HTMLCanvasElement;  //Id of the table
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      let imgWidth = 208;   
+      let pageHeight = 295;    
+      let imgHeight = canvas.height * imgWidth / canvas.width;  
+      let heightLeft = imgHeight;  
+
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      let position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('MYPdf.pdf'); // Generated PDF   
+    }); 
+  }
 }
+
