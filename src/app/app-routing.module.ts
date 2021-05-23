174dce5c64be2e9c333877a99f31ed72268a7c1d
RochDanistan <rochdanistan123@gmail.com>
2021-05-23 21:43:02 +0530

    added forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/login/login.component';
 import { ResumeComponent } from './resume/resume.component';
+import { ContactInfoComponent } from './user/contact-info/contact-info.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path:'login', component:LoginComponent},
-  { path:'resume', component:ResumeComponent}
+  { path:'resume', component:ResumeComponent},
+  { path:'useredit', component:ContactInfoComponent}
 ];
 
 @NgModule({
